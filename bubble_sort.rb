ea9690d2ee12638195a023ba37767c0000de3848
A L V A R O <alvarop3000@gmail.com>
2020-03-30 17:22:40 -0500

    correct stickler errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,8 @@
 def bubble_sort(arr)
   (0..arr.length - 1).each do |a|
     ((a + 1)..arr.length - 1).each do |b|
-      if arr[a] > arr[b]
-      arr[a], arr[b] = arr[b], arr[a]
+      arr[a], arr[b] = arr[b], arr[a] if arr[a] > arr[b] 
       end
-    end
   end
   p arr
 end
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,14 +2,14 @@ def bubble_sort_by(arr)
   loop do
     swap = false
     (arr.length - 1).times do |i|
-      if (arr[i].length <=> arr[i + 1].length).postive?
+      if (arr[i].length <=> arr[i + 1].length).positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swap = true
       end
     end
-    break if not swap
+    break unless swap
   end
   arr
 end
 
-puts bubble_sort_by([%^hi^, %^football^, %^rugby^, %^hello^, %^hey^])
+puts bubble_sort_by(%w[hi footbal rugby hello hey])
