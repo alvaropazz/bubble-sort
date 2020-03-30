779157b6adf2489118614f044e96b15d809dd339
A L V A R O <alvarop3000@gmail.com>
2020-03-30 17:25:48 -0500

    correct stickler errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,7 @@
 def bubble_sort(arr)
   (0..arr.length - 1).each do |a|
     ((a + 1)..arr.length - 1).each do |b|
-      arr[a], arr[b] = arr[b], arr[a] if arr[a] > arr[b] 
+      arr[a], arr[b] = arr[b], arr[a] if arr[a] > arr[b]
     end
   end
   p arr
