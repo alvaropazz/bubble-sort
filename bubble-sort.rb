021ae2f259e14fd3f49abba8a8028071f7670151
A L V A R O <alvarop3000@gmail.com>
2020-03-30 16:11:05 -0500

    add first loop

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,17 +1,11 @@
 def bubble_sort(arr)
-  (arr.length-1).times do
-    swapped = false
-    #loops through array each pass 
-    (arr.length-1).times do |i|
-        if arr[i]>arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-        swapped = true #var stores whether or not a swap's been made
-        else swapped = false
-        end
-      end
-    #end looping through array
-    break arr if swapped == false #break looping through arr when swapped doesn't change anymore 
+  (arr.length-1).times do |i|
+    if arr[i] > arr[i+1] then 
+      arr[i], arr[i+1] = arr[i+1], arr[i]
+      swapped = true
+    end
   end
+arr
 end
 
-puts bubble_sort([1,3,9,6,99,100,32,7])
+puts bubble_sort([100,200,99,32,9,8,6,5,4,3])
