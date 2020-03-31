47690401db8ddf31713a9ca708b8a28e55ccf663
A L V A R O <alvarop3000@gmail.com>
2020-03-30 20:15:58 -0500

    add yield

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,12 +2,13 @@ def bubble_sort_by(arr)
   loop do
     swap = false
     (arr.length - 1).times do |i|
-      if yield (arr[i].length <=> arr[i + 1].length).positive?
+      if (arr[i].length <=> arr[i + 1].length).positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swap = true
       end
     end
     break unless swap
   end
-  arr
+  yield arr
 end
+
